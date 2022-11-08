0bcb427d8f421676a34b13636f14561c1539dd80
itwill07 <blacksugars2s2@gmail.com>
2022-11-08 09:22:24 +0900

    1107

--- JAVA1107/src/nested_class/Ex3.java
+++ JAVA1107/src/nested_class/Ex3.java
@@ -1,87 +1,81 @@
-package netsted_class;
-
-import java.security.PublicKey;
+package nested_class;
 
 public class Ex3 {
 
 	public static void main(String[] args) {
 		/*
-		 * 익명(annoymous) 객체
+		 * 익명(anonymous) 객체
 		 * - 이름이 없는 객체
 		 * - 클래스 선언과 객체의 생성을 동시에 하기 때문에 단 한번만 사용될 수 있고
 		 *   오직 하나의 객체만을 생성할 수 있는 일회용 클래스
 		 * - 이름이 없기 때문에 생성자도 가질 수 없다.
-		 * - 부모클래스의 이름이나 구현하고자 하는 인터페이스의 이름을 사용해서 정의하므로 
-		 *   하나의 클래스로 상속받는 동시에 인터페이스를 구현하거나
+		 * - 부모클래스의 이름이나 구현하고자 하는 인터페이스의 이름을 사용해서 
+		 *   정의하므로 하나의 클래스로 상속받는 동시에 인터페이스를 구현하거나
 		 *   하나 이상의 인터페이스를 구현할 수 없다.
-		 * - 단 하나의 클래스를 상속받거나 단 하나의 인터페이스만을 구현할 수 있다
+		 * - 단 하나의 클래스를 상속받거나 단 하나의 인터페이스만을 구현할 수 있다.
 		 * 
 		 * < 익명 객체 문법 >
-		 *  new 슈퍼클래스이름() {
-		 *   	// 멤버 선언
-		 *  };
-		 *  
-		 *  또는
-		 *  
-		 *  new 구현인터페이스이름() {
-		 *  	// 멤버 선언
-		 *  };
+		 * new 슈퍼클래스이름() {
+		 * 		// 멤버 선언
+		 * }; 
+		 * 
+		 * 또는
+		 * 
+		 * new 구현인터페이스이름() {
+		 * 		// 멤버 선언
+		 * };
+		 * 
 		 */
-
 		Parent p = new Child(); // 업캐스팅
 		p.wake();
+		// => Child 의 오버라이딩 된 메서드 wake() 호출
 		
 		Child c = new Child();
-		c.method2(p);
-		c.method2(new Child2());
+//		c.method2(p);
 		
-		System.out.println("-------------------------------------------------------");
+		System.out.println("------------------------");
 		
-//		Child2 c2 = new Child2();
-////		c.method2(new Child2());
-//		c.method2(c2);
-//		// => 매개변수의 타입이 Parent 이므로 Parent 타입 p 또는
-//		//    서브 클래스인 Child, Child2 객체를 파라미터 값으로 활용 가능
+		AnonymousClass anony = new AnonymousClass();
+		// => 익명클래스를 활용하기 위해 AnonymousClass의 인스턴스 생성
 		
-		System.out.println("-------------------------------------------------------");
-		
-		AnnonymousClass annoy = new AnnonymousClass();
-		// => 익명의 클래스를 활용하기 위해 AnnoymousClass의 인스턴스 생성
-		annoy.p.wake(); // Child 클래스를 따로 두지 않아서 heap 메모리에 남지 않음
-		// => 익명 클래스 생성 및 메서드 호출
+		anony.p.wake();
+		// => 익명클래스 생성 및 메서드 호출
 		
 		System.out.println();
 		
-		Parent parent = annoy.p; // p는 Parent 타입이므로 Parent 타입 변수
+		Parent parent = anony.p; // p 는 Parent 타입이므로 Parent 타입 변수에 저장 가능
 		parent.wake();
 		
 		System.out.println();
 		
-		System.out.println("======================================");
+		System.out.println("=============================");
 		
 //		C c = new C();
 		// => 지역 내부 클래스는 객체를 바로 생성할 수 없음!
 		// => 즉, C 클래스를 사용하려면 클래스를 직접 생성하는 것이 아니라
 		//    method1() 메서드 호출을 통해 생성된 객체를 리턴(반환) 받아야 함
-		Parent parent2 = annoy.method1(); // 리턴 타입이 Parent 이므로 Parent 타입 변수
+		Parent parent2 = anony.method1(); // 리턴 타입이 Parent 이므로 Parent 타입 변수에 저장
 		parent2.wake();
 		
 		System.out.println();
 		
-		Parent parent3 = annoy.annoyMethod1();
+		Parent parent3 = anony.anonyMethod1();
 		parent3.wake();
 		
-		System.out.println("====================================");
+		System.out.println("======================================");
 		Child2 c2 = new Child2();
 //		c.method2(new Child2());
 		c.method2(c2);
 		// => 매개변수의 타입이 Parent 이므로 Parent 타입 p 또는
-		//    서브 클래스인 Child, Child2 객체를 파라미터 값으로 활용 가능
-		annoy.method2(
+		//    서브클래스인 Child, Child2 객체를 파라미터 값으로 활용 가능
+		
+		// method2의 파라미터값을 넘길 때 익명 클래스 활용
+		anony.method2(
 				new Parent() {
 					public void study() {
 						System.out.println("공부합니다!");
 					}
+
 					@Override
 					public void wake() {
 						System.out.println("4시에 일어납니다!");
@@ -90,8 +84,9 @@ public class Ex3 {
 				}
 				);
 		
-		System.out.println("================================================");
-		// =============================================================================
+		System.out.println("=======================================");
+		
+		// ==========================================================
 		// 인터페이스 익명 객체 활용
 //		I i = new I();
 		// => 인터페이스는 생성자가 요소에 없어서 인스턴스를 만들 수 없다!
@@ -104,10 +99,8 @@ public class Ex3 {
 		};
 		
 		i.wake();
-		
-		
-		
 	}
+
 }
 
 class Parent {
@@ -130,6 +123,7 @@ class Child extends Parent {
 	public void method2(Parent p) {
 		p.wake();
 	}
+	
 }
 
 class Child2 extends Parent {
@@ -145,38 +139,41 @@ class Child2 extends Parent {
 }
 
 // 익명 객체 활용
-class AnnonymousClass {
-	// AnnoymousClass 클래스의 필드 선언
+class AnonymousClass {
+	// AnonymounClass 클래스의 인스턴스 변수(=필드) 에서의 익명 클래스 활용
 	Parent p = new Parent() {
 		public void work() {
 			System.out.println("출근합니다!");
 		}
-		
+				
+		@Override
 		public void wake() {
 			System.out.println("6시에 일어납니다!");
 			work();
 		}
 	};
 	
-	// AnnoymousClass 클래스의 인스턴스 메서드에서의 익명 클래스 활용
-	// 지역 내부 클래스 사용
+	// AnonymounClass 클래스의 인스턴스 메서드 에서의 익명 클래스 활용
+	// 지역 내부 클래스 활용
 	public Parent method1() { // 메서드의 리턴타입이 Parent
-		class C extends Parent{ // Parent 타입의 객체를 반환하기 위해 메서드 내부에 클래스 정의
-								// Parent 클래스를 상속받는 C 클래스 정의
+		class C extends Parent { // Parent 타입의 객체를 반환하기 위해 
+								 // 메서드 내부에 클래스 정의
+								 // Parent 클래스를 상속 받는 C 클래스 정의
 			public void walk() {
 				System.out.println("강아지와 함께 산책합니다!");
 			}
 			
+			@Override
 			public void wake() {
 				System.out.println("5시에 일어납니다!");
 				walk();
-			}
+			}			 
 		}
-		return new C(); // C 클래스를 선언하고 리턴(반환) 함
+		return new C(); // C 클래스를 생성하고 리턴(반환) 함
 	}
-	// =>  위의 지역 내부 클래스를 익명 내부 클래스로 변경하면 아래와 같음!
-	
-	public Parent annoyMethod1() {
+	// => 위의 지역 내부 클래스를 익명 내부 클래스로 변경하면 아래와 같음!!
+		
+	public Parent anonyMethod1() {
 		return new Parent() {
 			public void walk() {
 				System.out.println("고양이와 함께 산책합니다!");
@@ -186,18 +183,25 @@ class AnnonymousClass {
 			public void wake() {
 				System.out.println("5시에 일어납니다!");
 				walk();
-			}
+			}			 
 		};
 	}
-	
+		
 	public void method2(Parent p) {
 		p.wake();
 	}
 	
 }
 
-// =================================================================================
+// ================================================================
 // 인터페이스 익명 객체
 interface I {
 	public abstract void wake();
 }
+
+
+
+
+
+
+
--- JAVA1107/src/nested_class/Ex4.java
+++ JAVA1107/src/nested_class/Ex4.java
@@ -1,4 +1,4 @@
-package netsted_class;
+package nested_class;
 
 public class Ex4 {
 
@@ -14,13 +14,14 @@ public class Ex4 {
 // 버튼의 클릭 이벤트 처리
 // Button 클래스 정의
 class Button {
-	static interface OnClickListener { // 중첩 인터페이스 선언
+	
+	static interface OnClickListner { // 중첩 인터페이스 선언
 		public abstract void onClick();
 	}
 	
-	OnClickListener listener; // 인터페이스 타입 변수(필드) 선언
+	OnClickListner listener; // 인터페이스 타입 변수(필드) 선언
 	
-	public void setOnClickListner(OnClickListener listener) { // 매개 변수의 다형성
+	public void setOnClickListner(OnClickListner listener) { // 매개 변수의 다형성
 		this.listener = listener;
 	}
 	// => 외부에서 구현 객체를 받아 필드에 대입
@@ -30,7 +31,6 @@ class Button {
 	}
 	// => 버튼 이벤트가 발생했을 때(touch() 메서드가 호출됐을 때)
 	//    인터페이스를 통해 구현 객체의 메서드를 호출(listener.onClick())
-
 }
 
 // Window 클래스를 Button 2개를 가지고 있는 창이라고 가정
@@ -41,16 +41,16 @@ class Window {
 	
 	// 필드 초기값으로 대입
 	// 필드값으로 익명 객체 대입
-	Button.OnClickListener listener = new Button.OnClickListener() {
+	Button.OnClickListner listener = new Button.OnClickListner() {
 		@Override
 		public void onClick() {
 			System.out.println("전화를 겁니다!");
 		}
-	};
+	};	
 	
 	public Window() { // 기본 생성자 정의
 		button1.setOnClickListner(listener);
-		button2.setOnClickListner(new Button.OnClickListener() {
+		button2.setOnClickListner(new Button.OnClickListner() {
 			@Override
 			public void onClick() {
 				System.out.println("메세지를 보냅니다!");
@@ -58,4 +58,16 @@ class Window {
 		});
 	}
 	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
